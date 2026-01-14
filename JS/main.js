c0c5f3dd6c1f5f04d66ddf1920ac6ae41ac83cab
ngoe <ngoeee0402@gmail.com>
2026-01-15 00:03:55 +0700

    1

--- JS/main.js
+++ JS/main.js
@@ -12,11 +12,21 @@
 
 // section-content
 
+const projectListBtn = document.querySelector(`.project-list-btn`);
+const projectList = document.querySelector(`.project-list`);
+const projectAddItem = document.querySelector(`.project-item.add`);
+const projectPinList = document.querySelector(`.project-pinList`);
+const tools = document.querySelector(`.tools`);
+const sectionList = document.querySelector(`.section-list`);
+const sectionAddItem = document.querySelector(`.section-item.add`);
+const sectionContent = document.querySelector(`.section-content`);
+
 const yewu1 = {
     name: "yewu",
     page: [
         {
             name: "page-1",
+            index: "1",
             content: [
                 {
                     type: "listToDo",
@@ -32,6 +42,12 @@ const yewu1 = {
         },
         {
             name: "page-2",
+            index: "2",
+            content: [
+                {
+                    // meow
+                },
+            ],
         },
     ],
 };
